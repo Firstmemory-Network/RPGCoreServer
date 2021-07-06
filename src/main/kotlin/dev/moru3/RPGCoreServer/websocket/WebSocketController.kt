95c70fca8afc6ae3e477a93c6a286fff0e1a353a
moru348 <moru348@gmail.com>
2021-07-07 01:06:42 +0900

    test

--- src/main/kotlin/dev/moru3/RPGCoreServer/websocket/WebSocketController.kt
+++ src/main/kotlin/dev/moru3/RPGCoreServer/websocket/WebSocketController.kt
@@ -1,20 +1,41 @@
 package dev.moru3.RPGCoreServer.websocket
 
-import org.springframework.stereotype.Controller
+import org.springframework.stereotype.Component
 import org.springframework.web.socket.CloseStatus
+import org.springframework.web.socket.PongMessage
 import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
 
-@Controller
+@Component
 class WebSocketController: TextWebSocketHandler() {
     val sessions = mutableListOf<WebSocketSession>()
 
-    override fun afterConnectionEstablished(session: WebSocketSession) { sessions.add(session) }
+    override fun handleTransportError(session: WebSocketSession, exception: Throwable) {
+        exception.printStackTrace()
+    }
 
-    override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) { sessions.remove(session) }
+    override fun handlePongMessage(session: WebSocketSession, message: PongMessage) {
+        println(message)
+    }
+
+    override fun afterConnectionEstablished(session: WebSocketSession) {
+        println("セッションが確立されました。")
+        println(session)
+        sessions.add(session)
+    }
+
+    override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
+        sessions.remove(session)
+        println("${session.remoteAddress?.hostName?:"null"} からのセッションが切断されました。")
+    }
 
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
+        println(Thread.currentThread().id)
+        println(message.toString())
+    }
 
+    init {
+        println("インスタンスが生成されました。")
     }
 }
